f8d72d815421b7673392a3f91004fe395e1087a5
zikwall <andrey.kapitonov.96@gmail.com>
2020-04-17 10:49:30 +0300

    Review comments

--- client/app/components/examples/Comment.js
+++ client/app/components/examples/Comment.js
@@ -7,45 +7,59 @@ const CommentExampleThreaded = () => (
         </Header>
 
         <Comment>
-            <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
+            <Comment.Avatar as='a' src='https://habrastorage.org/getpro/habr/avatars/791/217/d31/791217d314e7458aef0f63497e212538.png' />
             <Comment.Content>
-                <Comment.Author as='a'>Matt</Comment.Author>
+                <Comment.Author as='a'>SashulyaK</Comment.Author>
                 <Comment.Metadata>
-                    <span>Today at 5:42PM</span>
+                    <span>вчера в 13:59</span>
                 </Comment.Metadata>
-                <Comment.Text>How artistic!</Comment.Text>
+                <Comment.Text>Вы правы, мы изначально этим пользовались. Но разница между соседними фреймами даёт ложные срабатывания в динамичных сценах. А ещё она не детектит переходы, когда шоты плавно перетекают друг в друга фейдами. В итоге пришлось перейти на нейронки.</Comment.Text>
                 <Comment.Actions>
-                    <a>Reply</a>
+                    <a>Ответить</a>
                 </Comment.Actions>
             </Comment.Content>
         </Comment>
 
         <Comment>
-            <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+            <Comment.Avatar as='a' src='https://habrastorage.org/getpro/habr/avatars/93f/965/c06/93f965c063c9c1910aac6271e3686cc9.jpg' />
             <Comment.Content>
-                <Comment.Author as='a'>Elliot Fu</Comment.Author>
+                <Comment.Author as='a'>GeneD88</Comment.Author>
                 <Comment.Metadata>
-                    <span>Yesterday at 12:30AM</span>
+                    <span>вчера в 23:56</span>
                 </Comment.Metadata>
                 <Comment.Text>
-                    <p>This has been very useful for my research. Thanks as well!</p>
+                    <p>А как подобный алгоритм справляется с такими фильмами, где присутствует сплит-экран, например как The Sisters Де Пальмы (2 экрана), Timecode Фиггиса (4 экрана), Charly Нельсона и тп?</p>
                 </Comment.Text>
                 <Comment.Actions>
-                    <a>Reply</a>
+                    <a>Ответить</a>
                 </Comment.Actions>
             </Comment.Content>
 
             <Comment.Group>
                 <Comment>
-                    <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
+                    <Comment.Avatar as='a' src='https://habrastorage.org/getpro/habr/avatars/791/217/d31/791217d314e7458aef0f63497e212538.png' />
                     <Comment.Content>
-                        <Comment.Author as='a'>Jenny Hess</Comment.Author>
+                        <Comment.Author as='a'>SashulyaK</Comment.Author>
                         <Comment.Metadata>
-                            <span>Just now</span>
+                            <span>Только что</span>
                         </Comment.Metadata>
-                        <Comment.Text>Elliot you are always so right :)</Comment.Text>
+                        <Comment.Text>Хороший воспрос) Если использовать только фичи видеоряда, то, скорее всего, не сработает. Но если добавить звук (или использовать только звук), это может решить проблему.</Comment.Text>
                         <Comment.Actions>
-                            <a>Reply</a>
+                            <a>Ответить</a>
+                        </Comment.Actions>
+                    </Comment.Content>
+                </Comment>
+
+                <Comment>
+                    <Comment.Avatar as='a' src='https://habrastorage.org/getpro/habr/avatars/279/fd4/4f1/279fd44f1aab1c22791c701977182439.png' />
+                    <Comment.Content>
+                        <Comment.Author as='a'>marsermd</Comment.Author>
+                        <Comment.Metadata>
+                            <span>вчера в 23:39</span>
+                        </Comment.Metadata>
+                        <Comment.Text>Хабр здорового человека, где снова пишут про разработку</Comment.Text>
+                        <Comment.Actions>
+                            <a>Ответить</a>
                         </Comment.Actions>
                     </Comment.Content>
                 </Comment>
@@ -53,22 +67,22 @@ const CommentExampleThreaded = () => (
         </Comment>
 
         <Comment>
-            <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+            <Comment.Avatar as='a' src='https://habrastorage.org/getpro/habr/avatars/279/fd4/4f1/279fd44f1aab1c22791c701977182439.png' />
             <Comment.Content>
-                <Comment.Author as='a'>Joe Henderson</Comment.Author>
+                <Comment.Author as='a'>marsermd</Comment.Author>
                 <Comment.Metadata>
-                    <span>5 days ago</span>
+                    <span>вчера в 23:39</span>
                 </Comment.Metadata>
-                <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
+                <Comment.Text>Какая приятная статья! Давненько такого на хабре не читал. Спасибо большое!</Comment.Text>
                 <Comment.Actions>
-                    <a>Reply</a>
+                    <a>Ответить</a>
                 </Comment.Actions>
             </Comment.Content>
         </Comment>
 
         <Form reply>
             <Form.TextArea />
-            <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+            <Button content='Отправить' labelPosition='left' icon='edit' primary />
         </Form>
     </Comment.Group>
 )
