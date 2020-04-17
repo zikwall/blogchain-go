4f3b1b813ebd457a66964443c9b8b868c1ba3e46
zikwall <andrey.kapitonov.96@gmail.com>
2020-04-17 13:15:10 +0300

    Review post layout

--- client/pages/post/[id].js
+++ client/pages/post/[id].js
@@ -12,7 +12,7 @@ import {
     Label,
     Button,
     Divider,
-    Segment
+    Segment, Icon
 } from "semantic-ui-react";
 import CommentExampleThreaded from "../../app/components/examples/Comment";
 import { CommonLayout } from "../../app/layouts";
@@ -136,63 +136,56 @@ const Post = () => {
                 <Grid>
                     <Ref innerRef={contextRef}>
                         <Grid.Row columns={2}>
-                            <Grid.Column width={13}>
+                            <Grid.Column width={12}>
+                                <Publisher
+                                    name='SashulyaK'
+                                    time='сегодня в 13:08'
+                                    avatar="https://habrastorage.org/getpro/habr/avatars/791/217/d31/791217d314e7458aef0f63497e212538.png"
+                                />
+                                <Header as='h1'>
+                                    Как мы научились делить видео на сцены с помощью хитрой математики
+                                </Header>
 
-                                    <Publisher
-                                        name='SashulyaK'
-                                        time='сегодня в 13:08'
-                                        avatar="https://habrastorage.org/getpro/habr/avatars/791/217/d31/791217d314e7458aef0f63497e212538.png"
-                                    />
-                                    <Header as='h1'>
-                                        Как мы научились делить видео на сцены с помощью хитрой математики
-                                    </Header>
+                                <TagBar tags={[
+                                    "Разработка под Arduino",
+                                    "Периферия",
+                                    "DIY или Сделай сам"
+                                ]} />
 
-                                    <TagBar tags={[
-                                        "Разработка под Arduino",
-                                        "Периферия",
-                                        "DIY или Сделай сам"
-                                    ]} />
+                                <p>
+                                    За 10 лет существования ivi мы собрали базу из 90000 видео разной длины, размера и качества. Каждую неделю появляются сотни новых. У нас есть гигабайты метаданных, которые полезны для рекомендаций, упрощают навигацию по сервису и настройку рекламы. Но извлекать информацию непосредственно из видео мы начали только два года назад.
+                                </p>
+                                <p>
+                                    В этой статье я расскажу, как мы разбираем фильмы на структурные элементы и зачем нам это нужно. В конце есть ссылка на репозиторий Github с кодом алгоритмов и примерами.
+                                </p>
 
-                                    <p>
-                                        За 10 лет существования ivi мы собрали базу из 90000 видео разной длины, размера и качества. Каждую неделю появляются сотни новых. У нас есть гигабайты метаданных, которые полезны для рекомендаций, упрощают навигацию по сервису и настройку рекламы. Но извлекать информацию непосредственно из видео мы начали только два года назад.
-                                    </p>
-                                    <p>
-                                        В этой статье я расскажу, как мы разбираем фильмы на структурные элементы и зачем нам это нужно. В конце есть ссылка на репозиторий Github с кодом алгоритмов и примерами.
-                                    </p>
+                                <ImageWrap
+                                    src="/images/tmp/k4psugpjprxnpen_xkfykzemrqg.png"
+                                />
 
-                                    <ImageWrap
-                                        src="/images/tmp/k4psugpjprxnpen_xkfykzemrqg.png"
-                                    />
+                                <p>
+                                    На монтаже кадры нарезают на группы, которые по задумке режиссёра меняют местами и склеивают обратно. Последовательность кадров от одной монтажной склейки до другой в английском языке называют термином shot. К сожалению, русская терминология неудачная, потому что в ней такие группы тоже называются кадрами. Чтобы не запутаться, давайте использовать английский термин. Только введём русскоязычный вариант:
+                                </p>
 
-                                    <p>
-                                        На монтаже кадры нарезают на группы, которые по задумке режиссёра меняют местами и склеивают обратно. Последовательность кадров от одной монтажной склейки до другой в английском языке называют термином shot. К сожалению, русская терминология неудачная, потому что в ней такие группы тоже называются кадрами. Чтобы не запутаться, давайте использовать английский термин. Только введём русскоязычный вариант:
-                                    </p>
+                                <ImageWrap src="https://habrastorage.org/webt/3d/ri/5k/3dri5kfs4v30-sjr5_erajncrda.png" />
 
-                                    <ImageWrap src="https://habrastorage.org/webt/3d/ri/5k/3dri5kfs4v30-sjr5_erajncrda.png" />
-                                    <p>
-                                        За 10 лет существования ivi мы собрали базу из 90000 видео разной длины, размера и качества. Каждую неделю появляются сотни новых. У нас есть гигабайты метаданных, которые полезны для рекомендаций, упрощают навигацию по сервису и настройку рекламы. Но извлекать информацию непосредственно из видео мы начали только два года назад.
-                                    </p>
+                                {
+                                    [...new Array(2)].map((v, k) => (
+                                        <p k={k}>
+                                            За 10 лет существования ivi мы собрали базу из 90000 видео разной длины, размера и качества. Каждую неделю появляются сотни новых. У нас есть гигабайты метаданных, которые полезны для рекомендаций, упрощают навигацию по сервису и настройку рекламы. Но извлекать информацию непосредственно из видео мы начали только два года назад.
+                                        </p>
+                                    ))
+                                }
 
-                                    <Tags />
-                                    <PublisherSegment />
+                                <Tags />
+                                <PublisherSegment />
 
-                                    <CommentExampleThreaded />
-
+                                <CommentExampleThreaded />
                             </Grid.Column>
-                            <Grid.Column width={3}>
+                            <Grid.Column width={4}>
+                                <CompanyInfo />
                                 <Sticky context={contextRef} offset={30}>
-                                    <Menu pointing secondary vertical fluid>
-                                        <Menu.Item
-                                            name='TODO'
-                                            active
-                                        />
-                                        <Menu.Item
-                                            name='TODO'
-                                        />
-                                        <Menu.Item
-                                            name='TODO'
-                                        />
-                                    </Menu>
+                                    <MoreOfAuthor />
                                 </Sticky>
                             </Grid.Column>
                         </Grid.Row>
@@ -202,6 +195,148 @@ const Post = () => {
         </CommonLayout>
     )
 };
+
+const InfoRow = ({ label, value }) => (
+    <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: '5px',
+        paddingBottom: '5px'
+    }}>
+        <div style={{
+            textAlign: 'left',
+            width: '49%'
+        }}>
+            <b>{ label }</b>
+        </div>
+        <div style={{
+            textAlign: 'left',
+            width: '49%'
+        }}>
+            <span>{ value }</span>
+        </div>
+    </div>
+);
+
+const CompanyInfo = () => (
+    <div style={{
+        backgroundColor: '#f7f7f7',
+        padding: '10px',
+        borderRadius: '5px',
+        marginBottom: '15px',
+    }}>
+        <Header as="h4">
+           Информация
+        </Header>
+        <Divider />
+        <div>
+            <InfoRow
+                label="Дата основания"
+                value="26.03.2010 г."
+            />
+            <InfoRow
+                label="Сайт"
+                value="ivi.ru"
+            />
+        </div>
+    </div>
+)
+
+const MoreLabels = ({ views, comments }) => (
+    <>
+        <Label basic>
+            <Icon name='eye' /> { views }
+        </Label>
+        <Label basic as="a">
+            <Icon name='comment' /> { comments }
+        </Label>
+    </>
+);
+
+const MoreItem = ({ labels, title, href }) => {
+    return (
+        <>
+            <Header as="h4">
+                <a href={href} style={{
+                    textDecoration: 'none',
+                    color: 'rgba(0,0,0,.87)'
+                }}>
+                    { title }
+                </a>
+            </Header>
+            <MoreLabels
+                views={labels.views}
+                comments={labels.comments}
+            />
+            <Divider />
+        </>
+    )
+};
+
+const MoreOfAuthor = () => (
+    <div style={{
+        backgroundColor: '#f7f7f7',
+        padding: '10px',
+        borderRadius: '5px'
+    }}>
+        <Header as="h4">
+            Еще от автора
+        </Header>
+        <Divider />
+        <div>
+            <MoreItem
+                title="Apple выпустила новый iPhone SE по цене от 40 000 ₽"
+                href="/post/13"
+                labels={{
+                    views: 25,
+                    comments: 23
+                }}
+            />
+            <MoreItem
+                title="Девочки сидят дома: регистрация новых вебкам-моделей выросла на 37—69%"
+                href="/post/13"
+                labels={{
+                    views: 14,
+                    comments: 6
+                }}
+            />
+            <MoreItem
+                title="Коллеги: и не друг, и не враг, а как?"
+                href="/post/13"
+                labels={{
+                    views: 45,
+                    comments: 55
+                }}
+            />
+            <MoreItem
+                title="Ликбез по респираторам. Помогает ли респиратор от заражения вирусом. Обзор 11 респираторов"
+                href="/post/13"
+                labels={{
+                    views: 30,
+                    comments: 16
+                }}
+            />
+            <MoreItem
+                title="Велотренажер #Самоизоляция или как угомонить ребенка на карантине"
+                href="/post/13"
+                labels={{
+                    views: 342,
+                    comments: 234
+                }}
+            />
+            <MoreItem
+                title="Велотренажер #Самоизоляция или как угомонить ребенка на карантине"
+                href="/post/13"
+                labels={{
+                    views: 342,
+                    comments: 234
+                }}
+            />
+        </div>
+    </div>
+);
 
 const Link = ({ href, title }) => (
     <a href={href} style={{
