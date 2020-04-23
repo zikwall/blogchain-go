45311389c7c1f598d471ef2c1e5d96bf844d98f5
zikwall <andrey.kapitonov.96@gmail.com>
2020-04-23 20:43:12 +0300

    :bug: Fix JWT auth

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -21,17 +21,19 @@ func JWT(c *fiber.Ctx) {
 		if len(header) == 2 {
 			tokenString = header[1]
 
-			token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 				}
 				return mySigningKey, nil
 			})
 
-			if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-				if uuid, ok := claims["uuid"]; ok {
-					// set new instance
-					userInstance, _ = user.FindById(int64(uuid.(float64)))
+			if err == nil {
+				if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+					if uuid, ok := claims["uuid"]; ok {
+						// set new instance
+						userInstance, _ = user.FindById(int64(uuid.(float64)))
+					}
 				}
 			}
 		}
