98f579bbd26f06dd73eb00d8f325ccb27b4baab2
zikwall <andrey.kapitonov.96@gmail.com>
2020-04-27 20:34:08 +0300

    Added routes to a separate file

---
+++ init.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/gofiber/cors"
+	"github.com/gofiber/fiber"
+	"github.com/zikwall/blogchain/actions"
+	"github.com/zikwall/blogchain/middlewares"
+)
+
+func InitRoutes(app *fiber.App) {
+	app.Use(cors.New(cors.Config{
+		Filter:           nil,
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: false,
+		ExposeHeaders:    nil,
+		MaxAge:           0,
+	}))
+
+	app.Static("/docs", "./public/docs")
+	app.Static("/uploads", "./public/uploads")
+
+	// Main endpoint group by `/api` prefix
+	api := app.Group("/api", middlewares.JWT)
+	api.Get("/", actions.HelloWorldAction)
+
+	v1 := api.Group("/v1")
+	v1.Get("/profile/:username", actions.Profile)
+
+	// content
+	v1.Get("/content/:id", actions.GetContent)
+	v1.Get("/contents", actions.GetContents)
+
+	// tag
+	v1.Get("/tags", actions.Tags)
+	v1.Get("/tag/:tag", actions.GetContents)
+
+	editor := api.Group("/editor", middlewares.Authorization)
+	editor.Get("/content/:id", actions.GetEditContent)
+	editor.Post("/content/add", actions.AddContent)
+	editor.Post("/content/update/:id", actions.UpdateContent)
+
+	// not usage JWT middleware in Login & Register endpoints
+	auth := app.Group("/auth", middlewares.Auth)
+	auth.Post("/register", actions.Register)
+	auth.Post("/login", actions.Login)
+	auth.Post("/logout", actions.Login)
+}
--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/gofiber/cors"
 	"github.com/gofiber/fiber"
 	"github.com/urfave/cli"
-	"github.com/zikwall/blogchain/actions"
 	service "github.com/zikwall/blogchain/di"
-	"github.com/zikwall/blogchain/middlewares"
 	"log"
 	"os"
 )
@@ -82,44 +79,8 @@ func main() {
 		})
 
 		app := fiber.New()
-		app.Use(cors.New(cors.Config{
-			Filter:           nil,
-			AllowOrigins:     []string{"*"},
-			AllowMethods:     []string{"*"},
-			AllowHeaders:     []string{"*"},
-			AllowCredentials: false,
-			ExposeHeaders:    nil,
-			MaxAge:           0,
-		}))
 
-		app.Static("/docs", "./public/docs")
-		app.Static("/uploads", "./public/uploads")
-
-		// Main endpoint group by `/api` prefix
-		api := app.Group("/api", middlewares.JWT)
-		api.Get("/", actions.HelloWorldAction)
-
-		v1 := api.Group("/v1")
-		v1.Get("/profile/:username", actions.Profile)
-
-		// content
-		v1.Get("/content/:id", actions.GetContent)
-		v1.Get("/contents", actions.GetContents)
-
-		// tag
-		v1.Get("/tags", actions.Tags)
-		v1.Get("/tag/:tag", actions.GetContents)
-
-		editor := api.Group("/editor", middlewares.Authorization)
-		editor.Get("/content/:id", actions.GetEditContent)
-		editor.Post("/content/add", actions.AddContent)
-		editor.Post("/content/update/:id", actions.UpdateContent)
-
-		// not usage JWT middleware in Login & Register endpoints
-		auth := app.Group("/auth", middlewares.Auth)
-		auth.Post("/register", actions.Register)
-		auth.Post("/login", actions.Login)
-		auth.Post("/logout", actions.Login)
+		InitRoutes(app)
 
 		err := app.Listen(fmt.Sprintf("%s:%d", host, port))
 
