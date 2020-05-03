9702e2466a838d244b4632c96d7fae6f75a77bad
zikwall <andrey.kapitonov.96@gmail.com>
2020-05-03 21:37:06 +0300

    Review routes

--- init.go
+++ init.go
@@ -21,29 +21,27 @@ func InitRoutes(app *fiber.App) {
 	app.Static("/docs", "./public/docs")
 	app.Static("/uploads", "./public/uploads")
 
-	// Main endpoint group by `/api` prefix
+	// main endpoint group by `/api` prefix
 	api := app.Group("/api", middlewares.JWT)
-	api.Get("/", actions.HelloWorldAction)
+	// not usage JWT middleware in Login & Register endpoints
+	auth := app.Group("/auth", middlewares.Auth)
+	// editor required authorization
+	editor := api.Group("/editor", middlewares.Authorization)
 
 	v1 := api.Group("/v1")
 	v1.Get("/profile/:username", actions.Profile)
-
-	// content
 	v1.Get("/content/:id", actions.GetContent)
 	v1.Get("/contents/:page?", actions.GetContents)
-
-	// tag
 	v1.Get("/tags", actions.Tags)
 	v1.Get("/tag/:tag/:page?", actions.GetContents)
+	auth.Post("/register", actions.Register)
+	auth.Post("/login", actions.Login)
+	auth.Post("/logout", actions.Login)
 
-	editor := api.Group("/editor", middlewares.Authorization)
 	editor.Get("/content/:id", actions.GetEditContent)
 	editor.Post("/content/add", actions.AddContent)
 	editor.Post("/content/update/:id", actions.UpdateContent)
 
-	// not usage JWT middleware in Login & Register endpoints
-	auth := app.Group("/auth", middlewares.Auth)
-	auth.Post("/register", actions.Register)
-	auth.Post("/login", actions.Login)
-	auth.Post("/logout", actions.Login)
+	// todo deprecated delete
+	api.Get("/", actions.HelloWorldAction)
 }
