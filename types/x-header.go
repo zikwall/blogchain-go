4399fa4bd9dbfaa73f68a0f8e2b37291ae650ef0
zikwall <andrey.kapitonov.96@gmail.com>
2020-05-17 13:48:04 +0300

    Added custom app X-Header(s)

---
+++ types/x-header.go
@@ -0,0 +1,37 @@
+package types
+
+import "github.com/gofiber/fiber"
+
+type XHeader struct {
+	XBlogchainApp string
+	XPlatform     string
+	XAppVersion   string
+	XDeviceName   string
+	XDeviceId     string
+}
+
+func NewXHeader(c *fiber.Ctx) XHeader {
+	x := XHeader{
+		XBlogchainApp: "",
+		XPlatform:     "",
+		XAppVersion:   "",
+		XDeviceName:   "",
+		XDeviceId:     "",
+	}
+
+	x.resolveHeaders(c)
+
+	return x
+}
+
+func (x *XHeader) IsBlogchainApp() bool {
+	return len(x.XBlogchainApp) != 0
+}
+
+func (x *XHeader) resolveHeaders(c *fiber.Ctx) {
+	x.XBlogchainApp = c.Get("X-Blogchain-App")
+	x.XPlatform = c.Get("X-Platform")
+	x.XAppVersion = c.Get("X-App-Version")
+	x.XDeviceName = c.Get("X-Device-Name")
+	x.XDeviceId = c.Get("X-Device-Id")
+}
