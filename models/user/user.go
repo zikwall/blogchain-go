2765c638a963d7442eebaa0b85d28aec7c6408cd
zikwall <andrey.kapitonov.96@gmail.com>
2020-07-14 21:12:08 +0300

    Code review after a long time

--- models/user/user.go
+++ models/user/user.go
@@ -18,6 +18,36 @@ type User struct {
 	Profile Profile
 }
 
+func NewUser() *User {
+	return &User{
+		Id:           0,
+		Username:     "",
+		Email:        "",
+		PasswordHash: "",
+		ConfirmedAt: sql.NullInt64{
+			Int64: 0,
+			Valid: false,
+		},
+		BlockedAt: sql.NullInt64{
+			Int64: 0,
+			Valid: false,
+		},
+		CreatedAt: sql.NullInt64{
+			Int64: 0,
+			Valid: false,
+		},
+		UpdatedAt: sql.NullInt64{
+			Int64: 0,
+			Valid: false,
+		},
+		RegistrationIp: sql.NullString{
+			String: "",
+			Valid:  false,
+		},
+		Profile: NewProfile(),
+	}
+}
+
 type PublicUser struct {
 	Id       int64         `json:"id"`
 	Username string        `json:"username"`
