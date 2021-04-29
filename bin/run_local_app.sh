54c7366851752a9a846f395c0e19db5262dfe73b
zikwall <andrey.kapitonov.96@gmail.com>
2021-04-29 21:59:46 +0300

    Code review

--- bin/run_local_app.sh
+++ bin/run_local_app.sh
@@ -7,7 +7,7 @@ go run . \
   --database-password 123456 \
   --database-name blogchain \
   --database-dialect mysql \
-  --clickhouse-address 194.35.48.20 \
+  --clickhouse-address ******* \
   --clickhouse-database blogchain \
   --clickhouse-user default \
   --debug
