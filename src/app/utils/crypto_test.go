ea00aeb13d97ea5d43de508713d4164768ae0ea0
zikwall <andrey.kapitonov.96@gmail.com>
2021-07-05 15:36:48 +0300

    Golang Lint:  string `my_pass` has 2 occurrences, make it a constant

--- src/app/utils/crypto_test.go
+++ src/app/utils/crypto_test.go
@@ -2,23 +2,23 @@ package utils
 
 import "testing"
 
+const MockPassword = "my_pass"
+
 func TestBlogchainPasswordCorrectness(t *testing.T) {
 	t.Run("it should be correct password hash and verify", func(t *testing.T) {
-		password := "my_pass"
-		hash, err := GenerateBlogchainPasswordHash(password)
+		hash, err := GenerateBlogchainPasswordHash(MockPassword)
 
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		if BlogchainPasswordCorrectness(string(hash), password) != true {
+		if BlogchainPasswordCorrectness(string(hash), MockPassword) != true {
 			t.Fatal("Error, the password was expected to be valid")
 		}
 	})
 
 	t.Run("it shoudl be invalid password and verify also", func(t *testing.T) {
-		password := "my_pass"
-		hash, err := GenerateBlogchainPasswordHash(password)
+		hash, err := GenerateBlogchainPasswordHash(MockPassword)
 
 		if err != nil {
 			t.Fatal(err)
